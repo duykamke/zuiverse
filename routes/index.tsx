d6c9b416ab492890351ecf6f8c08cc078b3460fc
Duy Nguyen <duykamke@gmail.com>
2022-07-01 21:20:55 +0700

    Added head meta

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,34 @@
 /** @jsx h */
-import { h } from "preact";
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
+import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
 import Header from "../islands/Header.tsx";
 import LeftNav from "../islands/LeftNav.tsx";
 import { tw } from "@twind";
 
-export default function Home() {
+export default function MainPage(props: PageProps) {
+  const TITLE = "zuiverse";
+  const DESCRIPTION = "just zui's stuff";
+
   return (
-    <div>
-      <div>
-        <Header />
+    <>
+      <Head>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={props.url.href} />
+      </Head>
+      <div> class={tw`flex flex-col min-h-screen`}
+        <div>
+          <Header />
+        </div>
+        <div class={tw`bg-neutral-500 flex flex-row py-4`}>
+          <LeftNav />
+        </div>
       </div>
-      <div class={tw`bg-neutral-500 flex flex-row py-4`}>
-        <LeftNav />
-      </div>
-    </div>
+    </>
   );
 }
